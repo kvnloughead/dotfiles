f8607e7aacc0d127d6c74613c938f12b3ddb0f58
kvnloughead <kvnloughead@gmail.com>
2022-03-07 19:11:54 -0500

    Update terminal and vscode configs

--- .config/get-configs.sh
+++ .config/get-configs.sh
@@ -1,63 +1,77 @@
-# Haven't figured out how to symlink with Windows files. Instead, I have copies # of the config files stored in ~/.dotfiles/program-name/.
+# I Haven't figured out how to symlink successfully between Windows and WSL.
+# So to keep my VSCode and Terminal config files in version control, I am
+# keeping copies of each config file in ~/.config/program-name/, and wrote some
+# code to help manage the copies. Don't judge.
 #
-# 1. To open the real versions for editing, `editor $name-of-config`
+# Basic Usage
+#
+# 1. To open the real versions for editing, `some-editor $name-of-config`
 # 2. To update the copy of the config file, run the relevant getter function
 #
-# When initially setting up a new computer, or any time you want to update the configs
-# from the copies, run the relevant setter command.
+# When initially setting up a new computer, or any time you want to update the
+# actual configs from the copies, run the relevant setter command.
 
-# windows_userdir is declared in ~/.bashrc
+# $windows_userdir is declared in ~/.bashrc
 terminalconfig="${windows_userdir}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
 vscodesettings="${windows_userdir}/AppData/Roaming/Code/User/settings.json"
 vscodekeybindings="${windows_userdir}/AppData/Roaming/Code/User/keybindings.json"
 
+# Overwrites copy of terminal config file with actual config file
 get_terminal_config() {
     echo -n "This will overwrite your copy of the terminal config file. Ok (y/n)? "
     read answer
-    if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
-        cp $terminalconfig ~/dotfiles/terminal/settings.json
+    if [ "$answer" != "${answer#[Yy]}" ] ;
+    then
+        cp $terminalconfig ~/.config/terminal/settings.json
     else
         echo Aborting
     fi
 }
 
+# Overwrites copy of vscode config files with actual config files
+# Effects both settings.json and keybindings.json
 get_vscode_config() {
     echo -n "This will overwrite your copies of the vscode config files. Ok (y/n)? "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        cp $vscodesettings ~/dotfiles/vscode/settings.json
-        cp $vscodekeybindings ~/dotfiles/vscode/keybindings.json
+        cp $vscodesettings ~/.config/vscode/settings.json
+        cp $vscodekeybindings ~/.config/vscode/keybindings.json
     else
         echo Aborting
     fi
 }
 
+# Overwrites copies of terminal and vscode config files with actual config files
 get_windows_configs() {
     get_terminal_config
     get_vscode_config
 }
 
+# Overwrites actual terminal config with your copies
 set_terminal_config() {
     echo -n "This will overwrite your active Terminal config file. Ok (y/n)? "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        cp ~/dotfiles/terminal/settings.json $terminalconfig
+        cp ~/.config/terminal/settings.json $terminalconfig
     else
         echo Aborting
     fi
 }
 
+# Overwrites actual vscode configs with your copies
+# Effects both settings.json and keybindings.json
 set_vscode_config() {
     echo -n "This will overwrite your active VSCode config files. Ok (y/n)? "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ] ;then
-        cp ~/dotfiles/vscode/settings.json $vscodesettings
-        cp ~/dotfiles/vscode/keybindings.json $vscodekeybindings
+        cp ~/.config/vscode/settings.json $vscodesettings
+        cp ~/.config/vscode/keybindings.json $vscodekeybindings
     else
         echo Aborting
     fi
 }
 
+# Overwrites actual terminal and vscode configs with your copies
 set_windows_configs() {
     set_terminal_config
     set_vscode_config
