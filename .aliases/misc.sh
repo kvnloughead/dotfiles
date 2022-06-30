254c11efe4eb01ec71757d0414ba9d12f2a95891
kvnloughead <kvnloughead@gmail.com>
2022-06-29 21:23:52 -0400

    Add bak function to create foo.bak for any file foo

--- .aliases/misc.sh
+++ .aliases/misc.sh
@@ -1,3 +1,7 @@
+# will check for permission before removing something
+# run `rm -f` to omit request for permission
+alias rm="rm -i"
+
 # navigation
 alias ~="cd ~"
 alias ..="cd .."
@@ -29,3 +33,10 @@ alias tree="tree -I node_modules"
 # micro
 alias micro-keybind='micro ~/.config/micro/bindings.json'
 alias micro-settings='micro ~/.config/micro/settings.json'
+
+# makes backup of file in the cwd, preserving the extension
+# turns foo.baz.ext into foo.baz.bak.ext
+bak() {
+  FILENAME=$1
+  echo "${FILENAME%.*}".bak."${FILENAME##*.}"
+}
