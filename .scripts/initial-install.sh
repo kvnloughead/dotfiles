e040c32187025d0c71cae5f2fc22f30c9d02666f
kvnloughead <kvnloughead@gmail.com>
2022-08-23 19:50:26 -0400

    Add npm aliases and update install script

--- .scripts/initial-install.sh
+++ .scripts/initial-install.sh
@@ -83,3 +83,6 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
 yarn
+
+# install typescript
+sudo apt install node-typescript
