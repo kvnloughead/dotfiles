eb1d698b047a182ed99b7f1db35db3453fc98fe6
Kevin Loughead <kvnloughead@gmail.com>
2023-01-02 10:01:05 -0500

    Add ls alias

--- .aliases/misc.sh
+++ .aliases/misc.sh
@@ -11,8 +11,6 @@ mkcd() {
 
 # convenience
 alias start="explorer.exe"
-alias lsa="ls -a"
-alias lsal="ls -al"
 alias cl="clear"
 alias src="source ~/.bashrc"
 
@@ -29,3 +27,21 @@ alias tree="tree -I node_modules"
 # micro
 alias micro-keybind='micro ~/.config/micro/bindings.json'
 alias micro-settings='micro ~/.config/micro/settings.json'
+
+# makes backup of file in the cwd, preserving the extension
+# turns foo.baz.ext into foo.baz.bak.ext
+bak() {
+  FILENAME=$1
+  echo "${FILENAME%.*}".bak."${FILENAME##*.}"
+}
+
+# Makes prompt "anonymous" by showing only $
+anon() { 
+  PS1="$ "
+}
+
+alias node-wash="rm -rf node_modules && rm package-lock.json"
+
+alias lsa="ls -a"
+alias lsl="ls -lhF"
+alias lsal="ls -alhF"
