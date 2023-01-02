288eb07d6838b8776bb5170a3126518b1fc2f469
Kevin Loughead <kvnloughead@gmail.com>
2023-01-02 17:56:45 -0500

    Add nvm to initial install script

--- .scripts/initial-install.sh
+++ .scripts/initial-install.sh
@@ -11,10 +11,6 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt install gh
 
-# install micro
-# curl https://getmic.ro | bash
-# sudo mv micro /usr/bin
-
 # install pip
 sudo apt install python3-pip
 
@@ -27,8 +23,8 @@ curl https://pyenv.run | bash
 # install watson
 pip3 install td-watson
 
-# make `dev` dir and clone some repos
-cd ~ && mkdir dev && cd dev
+# make `projects` dir and clone some repos
+cd ~ && mkdir projects && cd projects
 git clone https://github.com/kvnloughead/blog
 git clone https://github.com/kvnloughead/command-line-notes
 cd command-line-notes && pip install -r requirements.txt
@@ -38,36 +34,35 @@ cd ~ && mkdir bin && cd bin
 git clone https://github.com/kvnloughead/command-line-notes
 cd command-line-notes && pip install -r requirements.txt
 
-# update npm
-sudo npm i -g npm
+# install nvm
+# https://learn.microsoft.com/en-us/windows/dev-environment/javascript/nodejs-on-wsl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+nvm install --lts
+nvm install node
 
 # interactive
 echo "Authorizing GitHub CLI"
 gh auth login
 
-# C utilities
-# sudo apt install clang
-# sudo apt install gdb  # for debugger
-
 # Ruby on Rails installation
 # [source](https://gorails.com/setup/ubuntu/18.04)
-sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+# sudo apt install curl
+# curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+# curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+# echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update
-sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
+# sudo apt-get update
+# sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
 
-cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+# cd
+# git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+# echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+# echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+# exec $SHELL
 
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+# git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+# echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+# exec $SHELL
 
 # After this, you still need to run install a ruby version and gems. Example:
 # rbenv install 3.1.2
