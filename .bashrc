e54be31a6288c56f944ce8877545a7c7a64d8532
Kevin Loughead <kvnloughead@gmail.com>
2023-01-18 19:45:23 -0500

    Fix display issue with `code` command

--- .bashrc
+++ .bashrc
@@ -1,4 +1,4 @@
-#sudo ln -s ../../bin/wslview /usr/local/bin/xdg-open
+# ~/.bashrc: executed by bash(1) for non-login shells.
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
--- .config/variables.sh
+++ .config/variables.sh
@@ -1,12 +1,8 @@
-export DISPLAY=localhost:0.0
-LS_COLORS="ow=01;36;40" && export LS_COLORS
 
 export dl='/mnt/c/Users/kvnlo/Downloads'
 export wh='/mnt/c/Users/kvnlo'
 export wc='/mnt/c'
 export gh='https://github.com/kvnloughead/'
 export lh='http://localhost'
-
-export EDITOR='nano'
-# export VISUAL='micro'
+export EDITOR='vim'
 
