18d1f79bc56a1f081464925141c2094d464adfb0
Kevin Loughead <kvnloughead@gmail.com>
2023-01-27 15:42:46 -0500

    Add custom `edit` action for todo.txt

--- .aliases/todotxt.sh
+++ .aliases/todotxt.sh
@@ -3,6 +3,7 @@ alias ta='todo.sh add'
 alias tl='todo.sh list'
 alias tla='todo.sh listall'
 alias td='todo.sh do' # completes item
+alias te='todo.sh edit' # custom action
 
 # removes by number. Can optionally remove terms from the task
 alias trm='todo.sh del' 
