d3b4327a3958e86e259ef32464d6ff32ab6205e7
Kevin Loughead <kvnloughead@gmail.com>
2023-01-27 12:35:35 -0500

    Add aliases for todo.txt

---
+++ .aliases/todotxt.sh
@@ -0,0 +1,8 @@
+alias t='todo.sh'
+alias ta='todo.sh add'
+alias tl='todo.sh list'
+alias tla='todo.sh listall'
+alias td='todo.sh do' # completes item
+
+# removes by number. Can optionally remove terms from the task
+alias trm='todo.sh del' 
